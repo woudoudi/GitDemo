60baed69a5c147529f25f57424ea8fa457e45835
1091312423@qq.com <1091312423@qq.com>
2019-04-04 13:39:13 +0800

    git first update new file

--- app/src/main/java/com/example/administrator/gitdemo/Test.java
+++ app/src/main/java/com/example/administrator/gitdemo/Test.java
@@ -1,7 +1,10 @@
 package com.example.administrator.gitdemo;
+
 /**
  * Created by Administrator on 2019/4/4 0004.
  */
 
-public    class Test   {
+public class Test {
+    private int t1;
+    private int t2;
 }
